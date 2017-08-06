d40b137bef81cc2cbb0de98d12cf4ef2b82abac0
Lin Tang <johntang1026@gmail.com>
2017-08-06 14:23:00 +0930

    tweak

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from em_matrix import EmMatrix
-from trainning_set import TrainningSet
-from base import db_conn
+from .em_matrix import EmMatrix
+from .trainning_set import TrainningSet
+from .base import db_conn
