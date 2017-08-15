46492604069546387e8ca6b1fbeeb4e0da9c2639
Lin Tang <johntang1026@gmail.com>
2017-08-15 21:07:54 +0930

    tweak gitignore file

---
+++ app/config/database.py
@@ -0,0 +1,25 @@
+database = {
+  'default': 'sentiment',
+  'sentiment': {
+    'driver': 'postgres',
+    'database': 'sentiment',
+    'host': '192.168.1.113',
+    'user': 'postgres',
+    'port': 5434,
+    'password': '',
+    'prefix': ''
+  }
+}
+
+# database = {
+#   'default': 'sentiment',
+#   'sentiment': {
+#     'driver': 'postgres',
+#     'database': 'sentiment',
+#     'host': '192.168.0.2',
+#     'user': 'postgres',
+#     'port': 5434,
+#     'password': '',
+#     'prefix': ''
+#   }
+# }
