076fb7eebeabb85037017cf5bf25b5962748635c
john <johntang1026@gmail.com>
2017-09-06 23:13:00 +0930

    add workers

--- app/api/training_task_controller.py
+++ app/api/training_task_controller.py
@@ -2,9 +2,11 @@ from flask_restful import Resource
 from ..lib import Batch, Trainer
 from ..config import batchSize, redis
 from task import init_worker, init_ps
+from celery import group
 
 class TrainingTaskController(Resource):
   def post(self):
     init_ps.apply_async(countdown = 5)
-    init_worker.apply_async(countdown = 5)
+    worker_tasks = group(init_worker.apply_async(countdown = 5) for i in xrange(8))()
+    worker_tasks.apply_async()
     return {"STATUS": "DONE"}
