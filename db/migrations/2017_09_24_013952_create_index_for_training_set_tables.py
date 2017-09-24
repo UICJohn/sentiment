6916338eb266085d1a08ce78dbfbeda25b9b12a7
john <johntang1026@gmail.com>
2017-09-24 11:14:10 +0930

    add index

---
+++ db/migrations/2017_09_24_013952_create_index_for_training_set_tables.py
@@ -0,0 +1,14 @@
+from orator.migrations import Migration
+
+
+class CreateIndexForTrainingSetTables(Migration):
+
+  def up(self):
+    with schema.table('training_sets') as table:
+      table.index('iterations')      
+      
+  def down(self):
+    """
+    Revert the migrations.
+    """
+    pass
