024c4f0b2da1d92229441de192f673d2eaeaa5cb
John <johntang1026@gmail.com>
2017-09-28 10:54:10 +0930

    tweak

--- app/lib/trainer.py
+++ app/lib/trainer.py
@@ -71,8 +71,8 @@ class Trainer(Base):
 
     print("run graph")
     #run graph
-    hooks=[tf.train.StopAtStepHook(last_step = 1000)]
-    with tf.train.MonitoredTrainingSession(master = server.target, is_chief=(self.task_index == 0), checkpoint_dir="/tmp", hooks=hooks, log_step_count_steps=100) as sess:
+    hooks=[tf.train.StopAtStepHook(last_step = 1500)]
+    with tf.train.MonitoredTrainingSession(master = server.target, is_chief=(self.task_index == 0), checkpoint_dir="/tmp", hooks=hooks) as sess:
       step_count = 0
       while not sess.should_stop():
         print("TASK %d FETCH DATA" % self.task_index)
@@ -82,7 +82,6 @@ class Trainer(Base):
           print("Task: %d - Step: %d" % (self.task_index, step_count))
           step_count += 1
         else:
-          print("Training Done")
-          break;
+          continue
           #request stop
       print("Training Done")
