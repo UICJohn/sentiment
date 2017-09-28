118694874aa78661ca33ef4c65dddd4ef3277fbf
John <johntang1026@gmail.com>
2017-09-28 11:02:52 +0930

    tweak

--- app/api/prediction_controller.py
+++ app/api/prediction_controller.py
@@ -3,15 +3,16 @@ from ..lib import Prediction
 from ..lib import Classifier
 
 class PredictionController(Resource):
-  def post(self):
-    parser = reqparse.RequestParser()
-    parser.add_argument('sentence')
-    args = parser.parse_args()
-		if args['sentence'] and Prediction.process(args['sentence']):
-			return {'status': 'DONE'}
-		else:
-			return 'ERROR', 404
+  # def post(self):
+  #   parser = reqparse.RequestParser()
+  #   parser.add_argument('sentence')
+  #   args = parser.parse_args()
+		# if args['sentence'] and Prediction.process(args['sentence']):
+		# 	return {'status': 'DONE'}
+		# else:
+		# 	return 'ERROR', 404
 
   # def get(self):
     #TODO predict input sentence
     # predicted()
+  pass
