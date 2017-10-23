81636417eb6a11af778d04346c0154e2c220d92e
Chih Yang <chih.sg@gmail.com>
2017-10-23 17:48:26 +1030

    tweak

--- app/api/training_set_controller.py
+++ app/api/training_set_controller.py
@@ -6,16 +6,16 @@ from task import init_ps
 
 class TrainingSetController(Resource):
 
-  # def get(self):
-  #   if redis.has('max_word_length'):
-  #     redis.get('max_word_length')
-  #   else:
-  #     TrainingSet.select(db_conn.raw("max(array_length(regexp_split_to_array(words, '\s|\t'), 1)) as max_len")).first().max_len
-  #     redis.put('max_word_length', max_word_length, 180)
-  #   set_info = {'Count': TrainingSet.count(), 'Maximum Word Length': redis.get('max_word_length'), 'Batch Size': batchSize, 'Batch Count': 2}
-  #   return set_info
+  def get(self):
+    if redis.has('max_word_length'):
+      redis.get('max_word_length')
+    else:
+      TrainingSet.select(db_conn.raw("max(array_length(regexp_split_to_array(words, '\s|\t'), 1)) as max_len")).first().max_len
+      redis.put('max_word_length', max_word_length, 180)
+    set_info = {'Count': TrainingSet.count(), 'Maximum Word Length': redis.get('max_word_length'), 'Batch Size': batchSize, 'Batch Count': 2}
+    return set_info
 
-  def post(self):
-    for i in range(0,1):
-      init_ps.delay()
-    return {"STATUS": "NONE"}
+  # def post(self):
+  #   for i in range(0,1):
+  #     init_ps.delay()
+  #   return {"STATUS": "NONE"}
