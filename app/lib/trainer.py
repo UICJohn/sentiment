20476e838b46b25b7f8b99d18e548513177f5953
zhi yang <zhiyang@zhis-iMac.local>
2017-10-25 21:47:53 +1030

    modify summary

--- app/lib/trainer.py
+++ app/lib/trainer.py
@@ -64,7 +64,7 @@ class Trainer(Base):
       tf.summary.histogram('bias', bias)
       summary_op = tf.summary.merge_all()
       logdir = "tensorBoard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
-      #summary_hook = tf.train.SummarySaverHook(save_secs=600,output_dir=logdir,summary_op=summary_op)
+      summary_hook = tf.train.SummarySaverHook(save_secs=600,output_dir=logdir,summary_op=summary_op)
       
       print("Done prepare tensorboard parameters")
 
@@ -72,7 +72,9 @@ class Trainer(Base):
         if(self.task_index == 0):
           print("CURRENT EPOCH: %d" % i)
         hooks=[tf.train.StopAtStepHook(last_step = 1000 * (i + 1))]
-        with tf.train.MonitoredTrainingSession(master = server.target, is_chief=(self.task_index == 0), checkpoint_dir= os.path.expanduser('~/sentiment/logs/'), hooks = hooks) as sess:
+        with tf.train.MonitoredTrainingSession(master = server.target, is_chief=(self.task_index == 0), checkpoint_dir= os.path.expanduser('~/sentiment/logs/'), hooks = [hooks, summary_hook]) as sess:
+
+        #with tf.train.MonitoredTrainingSession(master = server.target, is_chief=(self.task_index == 0), checkpoint_dir= os.path.expanduser('~/sentiment/logs/'), hooks = hooks) as sess:
           step_count = 0
           print("Start to summary graph")
           writer = tf.summary.FileWriter(logdir, sess.graph)
@@ -85,11 +87,11 @@ class Trainer(Base):
             step_count += 1
             
             print("Start to summary ops")
-            if (i%100 == 0 and i > 0):
-              summary = sess.run(summary_op, {input_data: data, labels:data_labels})
-              print("Start to add summary files")
-              writer.add_summary(summary, i)
-            writer.close()              
+            # if (i%100 == 0 and i > 0):
+            #   summary = sess.run(summary_op, {input_data: data, labels:data_labels})
+            #   print("Start to add summary files")
+            #   writer.add_summary(summary, i)
+            # writer.close()              
           print("%d Training Done" % self.task_index)
           
 
