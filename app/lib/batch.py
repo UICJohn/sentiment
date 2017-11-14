898160c1496ea122652768ca2ce80a0c5a8ce90b
John <johntang1026@gmail.com>
2017-11-14 10:49:27 +1030

    add batch steps cont function

--- app/lib/batch.py
+++ app/lib/batch.py
@@ -34,23 +34,8 @@ class Batch(Base):
     elif(int(batch_count) < maxBatchCount):
       return True
     else:
-      return False  
-  # def __vector2matrix(cls, training_sets, max_sentence_len):
-  #   labels = []
-  #   data = []
-  #   for training_set in training_sets:
-  #     matrix = []
-  #     label = [0] * 3
-  #     word_ids = training_set.word_ids
-  #     for word_id in word_ids:
-  #       word = EmMatrix.where('id', word_id).first()
-  #       if word:
-  #         matrix.append(word.vector)
-  #       else:
-  #         matrix.append([0] * 300)
-  #     for l in range(len(matrix), max_sentence_len):
-  #       matrix.append([0] * 300)
-  #       label[training_set.label + 1] = 1
-  #     data.append(matrix)
-  #     labels.append(label)
-  #   return [data, labels]
+      return False
+
+  @classmethod
+  def steps_per_epoch(cls):
+    return int(TrainingSet.count()/batchSize)
